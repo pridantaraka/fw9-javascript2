664f01b075951aec833e8b5af154e8530f21c6ed
pridantaraka <pridantaraka@gmail.com>
2022-06-22 07:59:45 +0700

    tambahan batasan

--- Task3_revisi.js
+++ Task3_revisi.js
@@ -1,14 +1,16 @@
     let dataArray = [2,25,4,14,17,30,8]
+    // 
     dataArray.sort((a, b) => a - b);
     function seleksi (nilaiAwal, nilaiAkhir) {
         let hasil = '';
-        if(nilaiAwal<nilaiAkhir){
+        if(dataArray.length < 5){
+            console.log('Jumlah angka dalam dataArray harus lebih dari 5');
+        }else if(nilaiAwal < nilaiAkhir){
             hasil = dataArray.filter(dataA => dataA > nilaiAwal && dataA < nilaiAkhir);
         }else{
-            console.log('tidak sesuai');
+            console.log('Nilai Akhir harus lebih besar dari 5');
         }
         return hasil;
     }
-
 
     console.log(seleksi(5,20));
